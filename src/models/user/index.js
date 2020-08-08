e076cd5da40b220a777fcc787981fe6fb307f0c3
suhailphp <suhailphp@gmail.com>
2020-08-08 15:29:08 +0400

    New User model created

---
+++ src/models/user/index.js
@@ -0,0 +1,38 @@
+"use strict";
+const appConfig = require("../../configurations/appConfig.js");
+const debug = require("debug")(appConfig.APP_NAME + ":Model:User");
+const Moment = require("moment");
+const Joi = require("joi");
+
+module.exports = (SequelizeDB, DataTypes) => {
+  let User = SequelizeDB.define("User", {
+    UserID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      schema: Joi.string().required().min(3).label("User Name"),
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      schema: Joi.string().email().required().label("User Email"),
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      schema: Joi.string().required().min(4).label("User Email"),
+    },
+  });
+
+  User.associate = (models) => {
+    /**
+     * With Users Table
+     */
+  };
+
+  return User;
+};
